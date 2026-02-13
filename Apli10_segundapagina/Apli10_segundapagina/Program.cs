e2e0e3e8963b174071abe1368db93fbc7a6daf75
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 18:13:20 -0400

    Ejercicio 10 segunda pagina

---
+++ Apli10_segundapagina/Apli10_segundapagina/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class BuscarEnLista
+{
+    static void Main()
+    {
+
+        int[] numeros = { 14, 25, 8, 42, 33, 10, 5, 77 };
+
+        Console.WriteLine("--- Buscador de Valores en Lista ---");
+        Console.WriteLine("Lista disponible: " + string.Join(", ", numeros));
+
+        Console.Write("\n¿Qué número deseas buscar? ");
+        int valorBuscado = Convert.ToInt32(Console.ReadLine());
+
+        bool encontrado = false;
+        int posicion = -1;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] == valorBuscado)
+            {
+                encontrado = true;
+                posicion = i; 
+                break; 
+            }
+        }
+
+        if (encontrado)
+        {
+            Console.WriteLine($"\n¡Éxito! El número {valorBuscado} está presente en la posición {posicion}.");
+        }
+        else
+        {
+            Console.WriteLine($"\nEl número {valorBuscado} NO se encuentra en la lista.");
+        }
+    }
+}
