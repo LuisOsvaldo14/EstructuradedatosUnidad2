2cf18ea38e6e9b6f3edc586b0ee27b8d63673768
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 17:37:13 -0400

    ejercicio4 segunda pagina.

---
+++ Apli4_segundaPagina/Apli4_segundaPagina/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SumaLista
+{
+    static void Main()
+    {
+
+        int[] numeros = { 10, 25, 5, 12, 8 };
+
+        int sumaTotal = 0;
+
+        Console.WriteLine("Suma de elementos de una lista");
+        Console.Write("Elementos en la lista: ");
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write(numeros[i] + " "); 
+            sumaTotal += numeros[i];         
+        }
+        Console.WriteLine($"La suma total de los elementos es: {sumaTotal}");
+    }
+}
