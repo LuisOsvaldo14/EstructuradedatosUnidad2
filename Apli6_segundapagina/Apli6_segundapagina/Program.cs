e9e87e25a75e5321af5cff04817bdb01675ac44b
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 17:50:31 -0400

    ejercicio 6 segunda pagina

---
+++ Apli6_segundapagina/Apli6_segundapagina/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class PromedioLista
+{
+    static void Main()
+    {
+
+        int[] numeros = { 15, 20, 35, 40, 10 };
+
+        int sumaTotal = 0;
+
+        Console.WriteLine("--- Cálculo del Promedio de una Lista ---");
+        Console.Write("Elementos: " + string.Join(", ", numeros));
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            sumaTotal += numeros[i];
+        }
+
+        double promedio = (double)sumaTotal / numeros.Length;
+        Console.WriteLine("\n");
+        Console.WriteLine($"Suma total: {sumaTotal}");
+        Console.WriteLine($"Cantidad de elementos: {numeros.Length}");
+        Console.WriteLine($"El promedio es: {promedio:F2}"); 
+    }
+}
