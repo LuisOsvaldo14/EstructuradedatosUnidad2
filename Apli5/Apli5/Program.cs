2f80f6bfdabd3a0dbf84376e9cde63d77461773b
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 16:34:57 -0400

    ejercicio 5 completo

---
+++ Apli5/Apli5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Mayor
+{
+    static void Main()
+    {
+        Console.WriteLine("Programa para encontrar el número mayor" );
+
+       
+        int mayor = int.MinValue;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Introduce el número {i}: ");
+            int numeroActual = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (numeroActual > mayor)
+            {
+                mayor = numeroActual;
+            }
+        }
+
+        
+        Console.WriteLine($"El número mayor de los 10 ingresados es: {mayor}");
+    }
+}
