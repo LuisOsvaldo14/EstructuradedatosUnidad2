b08b418bc4fad70ef1778f12a1b222591c6cdd44
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 16:24:05 -0400

    programa 4

---
+++ Apli4/Apli4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class EjercicioMCM
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Calculadora de Mínimo Común Múltiplo (MCM) ---");
+
+
+        Console.Write("Introduce el primer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int mcm;
+
+        if (num1 > num2)
+        {
+            mcm = num1;
+        }
+        else
+        {
+            mcm = num2;
+        }
+
+        
+        while (true)
+        {
+            if (mcm % num1 == 0 && mcm % num2 == 0)
+            {
+                
+                Console.WriteLine($"El MCM de {num1} y {num2} es: {mcm}");
+                break; 
+            }
+
+            mcm++;
+        }
+    }
+}
