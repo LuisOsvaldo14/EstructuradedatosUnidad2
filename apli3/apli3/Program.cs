e216fc0e0958fe800c2a2a0c309d2fa6c28d567b
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 16:15:58 -0400

    apli 3 subida

---
+++ apli3/apli3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+class contarpares
+{
+    static void Main()
+    {
+        Console.WriteLine("Este es un programa que solicita 5 numeros al usuario y calcula su media aritmetica");
+
+
+        int suma = 0;
+        int acumulador = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Introduce tu numero{i}: ");
+            int Numero = Convert.ToInt32(Console.ReadLine());
+            acumulador = acumulador + Numero; 
+        }
+        int media = acumulador / 5;
+        Console.WriteLine($"La media aritmetica de los 5 numeros que introdugiste es {media}");
+    }
+}
