2af494adaf694f86035493d2afd7367c04bad4c7
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 18:02:00 -0400

    ejercicio 8 segunda pagina

---
+++ Apli8_segundapagina/Apli8_segundapagina/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class InvertirLista
+{
+    static void Main()
+    {
+
+        int[] original = { 1, 2, 3, 4, 5 };
+        int[] invertida = new int[original.Length];
+
+        Console.WriteLine("Inversión de una Lista");
+        Console.WriteLine("Original: " + string.Join(", ", original));
+
+        int j = original.Length - 1;
+        for (int i = 0; i < original.Length; i++)
+        {
+            invertida[j] = original[i];
+            j--; 
+        }
+
+        Console.WriteLine("Invertida: " + string.Join(", ", invertida));
+    }
+}
