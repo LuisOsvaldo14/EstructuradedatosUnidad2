1a847c1ef3437af8b1c37c7b3659ff9942dcac2d
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 17:32:16 -0400

    ejercicio 3

---
+++ Apli3_segundapagina/Apli3_segundapagina/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class PalindromoPalabra
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Verificador de Palíndromos ---");
+        Console.Write("Introduce una palabra: ");
+
+        string entrada = Console.ReadLine().ToLower().Replace(" ", "");
+
+        string invertida = "";
+
+        for (int i = entrada.Length - 1; i >= 0; i--)
+        {
+            invertida += entrada[i];
+        }
+
+        if (entrada == invertida && entrada != "")
+        {
+            Console.WriteLine($"La palabra es palíndroma.");
+            Console.WriteLine($"Original: {entrada} | Al revés: {invertida}");
+        }
+        else
+        {
+            Console.WriteLine("La palabra NO es palíndroma.");
+        }
+    }
+}
