49ad5fe2379e8f98e1d74369f1906795e1afe8b4
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 19:07:14 -0400

    ejercicio 2 estructuradatos

---
+++ Estructuradatos_2_RotacionArreglo/Estructuradatos_2_RotacionArreglo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class RotacionArreglos
+{
+    static void Main()
+    {
+
+        int[] arreglo = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Arreglo Original: " + string.Join(", ", arreglo));
+
+        Console.Write("¿Cuántas posiciones desea rotar?: ");
+        int posiciones = int.Parse(Console.ReadLine());
+
+        Console.Write("Dirección (derecha/izquierda): ");
+        string direccion = Console.ReadLine().ToLower();
+
+        int n = arreglo.Length;
+        int[] resultado = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (direccion == "derecha")
+            {
+
+                int nuevaPosicion = (i + posiciones) % n;
+                resultado[nuevaPosicion] = arreglo[i];
+            }
+            else 
+            {
+
+                int nuevaPosicion = (i - posiciones % n + n) % n;
+                resultado[nuevaPosicion] = arreglo[i];
+            }
+        }
+
+        Console.WriteLine($"Arreglo Rotado ({direccion}): " + string.Join(", ", resultado));
+    }
+}
