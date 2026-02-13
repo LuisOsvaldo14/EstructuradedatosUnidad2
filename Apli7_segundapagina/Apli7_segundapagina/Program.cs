f7e642b12a8492235ec4e140e0e89dd59ecf9536
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 17:56:07 -0400

    ejercicio 7 segunda pagina hecho

---
+++ Apli7_segundapagina/Apli7_segundapagina/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ListaDuplicada
+{
+    static void Main()
+    {
+        int[] listaOriginal = { 5, 12, 8, 20, 3 };
+        int[] listaNueva = new int[listaOriginal.Length];
+
+        Console.WriteLine("Multiplicar elementos por 2");
+        Console.WriteLine("Lista Original: " + string.Join(", ", listaOriginal));
+
+        for (int i = 0; i < listaOriginal.Length; i++)
+        {
+            listaNueva[i] = listaOriginal[i] * 2;
+        }
+
+        Console.WriteLine("Lista Nueva (x2): " + string.Join(", ", listaNueva));
+    }
+}
