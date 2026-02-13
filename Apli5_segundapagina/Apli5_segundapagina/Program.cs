c4f4df68322aabf2bfd5de7f54d8c97be4eecab4
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 17:44:39 -0400

    ejercicio 5 segunda pagina

---
+++ Apli5_segundapagina/Apli5_segundapagina/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class MaximoMinimoLista
+{
+    static void Main()
+    {
+        int[] numeros = { 45, 12, 89, 3, 27, 56, -5, 100 };
+
+        int maximo = numeros[0];
+        int minimo = numeros[0];
+
+        Console.WriteLine("--- Búsqueda de Máximo y Mínimo ---");
+        Console.WriteLine("Lista de números: " + string.Join(", ", numeros));
+
+        for (int i = 1; i < numeros.Length; i++)
+        {
+
+            if (numeros[i] > maximo)
+            {
+                maximo = numeros[i];
+            }
+
+            if (numeros[i] < minimo)
+            {
+                minimo = numeros[i];
+            }
+        }
+
+
+        Console.WriteLine($"El valor MÁXIMO es: {maximo}");
+        Console.WriteLine($"El valor MÍNIMO es: {minimo}");
+    }
+}
