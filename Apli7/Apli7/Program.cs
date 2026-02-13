4d0d4b9e8931980258b233a3a8d8554b74c2c284
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 16:55:06 -0400

    apli 7 hecha

---
+++ Apli7/Apli7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Multiplos
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Múltiplos de 5 entre dos números ---");
+        Console.Write("Introduce el primer número: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduce el segundo número: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        int inicio = Math.Min(n1, n2);
+        int fin = Math.Max(n1, n2);
+        Console.WriteLine($"\nMúltiplos de 5 entre {inicio} y {fin}:");
+        bool encontroAlguno = false;
+
+        
+        for (int i = inicio; i <= fin; i++)
+        {
+            
+            if (i % 5 == 0)
+            {
+                Console.WriteLine(i);
+                encontroAlguno = true;
+            }
+        }
+
+        if (!encontroAlguno)
+        {
+            Console.WriteLine("No se encontraron múltiplos de 5 en este rango.");
+        }
+    }
+}
