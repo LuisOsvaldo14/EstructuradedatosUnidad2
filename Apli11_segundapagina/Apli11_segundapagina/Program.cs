772003e9fb170fbc4bb4cf0538b8f5a85b960cab
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-13 18:23:34 -0400

    Ejercicio 11 segunda pagina

---
+++ Apli11_segundapagina/Apli11_segundapagina/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class BuscarPosicion
+{
+    static void Main()
+    {
+
+        string[] frutas = { "Manzana", "Pera", "Uva", "Sandía", "Mango" };
+
+        Console.WriteLine("--- Buscador de Posiciones (Índices) ---");
+        Console.WriteLine("Lista: " + string.Join(", ", frutas));
+
+        Console.Write("\nEscribe el nombre de la fruta para saber su posición: ");
+        string buscar = Console.ReadLine();
+
+        int posicionEncontrada = -1; 
+
+        for (int i = 0; i <= frutas.Length; i++)
+        {
+            
+            if (frutas[i].Equals(buscar, StringComparison.OrdinalIgnoreCase))
+            {
+                posicionEncontrada = i;
+                break; 
+            }
+        }
+
+        if (posicionEncontrada != -1)
+        {
+            Console.WriteLine($"\nLa fruta '{buscar}' está en la posición de índice: {posicionEncontrada}");
+        }
+        else
+        {
+            Console.WriteLine("\nEse elemento no existe en la lista.");
+        }
+    }
+}
